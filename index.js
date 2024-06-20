dec1e8001c05a3042173a5bd000b73915040ad63
lorenzzhh <lorenz.fritschi@gmx.ch>
2024-06-20 16:41:39 +0200

    connection to database

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const express = require('express');
 const {connectToDb} = require("./database");
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
-//const morgan = require('morgan');
-
-/*// Middleware
-app.use(morgan('dev'));
-app.use(express.json());*/
-
+app.use(cors())
 
 // Route zum Abrufen aller Spieler
 app.get('/api/players', async (req, res) => {
@@ -48,6 +44,61 @@ app.get('/api/games', async (req, res) => {
 });
 
 
+app.get('/api/players/:search', async (req, res) => {
+    try {
+
+        const db = await connectToDb();
+        const players = db.collection("player")
+
+        res.json(await players.find({
+            "name": {
+                $regex: req.params.search,
+                $options: "i"
+            }
+        }).toArray());
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.get('/api/players/sorted/:art/:type', async (req, res) => {
+    console.log("sort")
+    try {
+
+        const db = await connectToDb();
+        const players = db.collection("player")
+
+
+
+        switch (req.params.art){
+            case "age":
+                res.json(await players.find({}).sort({age: req.params.type}).toArray());
+                break;
+            case "marketValue":
+                res.json(await players.find({}).sort({market_value: req.params.type}).toArray());
+                break;
+            case "name":
+                res.json(await players.find({}).sort({name: req.params.type}).toArray());
+                break;
+            case "team":
+                res.json(await players.find({}).sort({team: req.params.type}).toArray());
+                break;
+            case "nationality":
+                res.json(await players.find({}).sort({nationality: req.params.type}).toArray());
+                break;
+            default:
+                break;
+        }
+
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 /*// Route zum Abrufen eines einzelnen Spielers
 app.get('/api/players/:id', async (req, res) => {
